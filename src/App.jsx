43b8d8f6e77f21bf356d221d21fab1cf8c00d9ef
Hugo VPS <hugojuggernot@gmail.com>
2026-05-18 01:22:33 +0200

    chore: remove portfolio section for now

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Services from './components/Services'
-import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
@@ -15,7 +14,6 @@ function App() {
         <Hero />
         <About />
         <Services />
-        <Portfolio />
         <Contact />
       </main>
       <Footer />
